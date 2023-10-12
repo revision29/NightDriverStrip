704a1925cdd247ef466ef5a5aae5f7a0cbf09698
Joe Schneider <joe@revision29.com>
2023-10-12 13:59:55 -0400

    Update remotecontrol.cpp
    Changed the user remote buttons to a map instead of vector. Fixing some repeat timing issues introduced when implementing my remote control code schema.

--- src/remotecontrol.cpp
+++ src/remotecontrol.cpp
@@ -36,6 +36,187 @@
 
 #define BRIGHTNESS_STEP     20
 
+UserRemoteControl myRemoteController (44);
+
+//Repeat codes
+//Press and hold brightness up or down to wuickly adjust up. 
+//Otherwise acknolwedge the keypress only every 200ms or.
+//First check for a match. If no metch we just return.
+//If a match then we check against repeating then we trigger the button action
+
+void RemoteControl::handle()
+{
+    decode_results results;
+    static int lastResult = 0;
+
+    static CRGB lastManualColor = CRGB(0,0,0);
+
+    auto& deviceConfig = g_ptrSystem->DeviceConfig();
+
+    if (!_IR_Receive.decode(&results))
+        return;
+
+    int result = results.value;
+    _IR_Receive.resume();
+
+auto searchResult = myRemoteController.buttons.find(result);
+    if (searchResult != myRemoteController.buttons.end()) 
+    {
+        debugI("We have a remote code result %08x", result);
+        uint buttonCount = myRemoteController.buttons.size();
+        debugI("There are %i buttons in the remote", buttonCount);
+        RemoteButton thisButton =  searchResult->second;
+        //Check for repeat
+        //myRemoteController.buttons[result]
+        /*
+        if (result == lastResult) 
+        {
+            static uint lastRepeatTime = millis();
+            const auto kMinRepeatms = (myRemoteController.buttons[result].buttonAction == BRIGHTNESS_DOWN || myRemoteController.buttons[result].buttonAction == BRIGHTNESS_UP) ? 150 : 250;
+            //We do not want to set the kMinRepeatms to 0 because some remotes send a code more than once and also there might be refelctive surfaces that will cause the signal to be recieved more than once in rapid succession.
+            //250 will allow 4 units brightness changes over one second. That will be about 3 seconds to fully ramp the brightnss up or down
+            if (millis() - lastRepeatTime > kMinRepeatms)
+            {
+                debugV("Remote Repeat; lastResult == %08x, elapsed = %lu\n", lastResult, millis()-lastRepeatTime);
+                lastRepeatTime = millis();
+            }
+            else
+            {
+                return;
+            }   
+
+        }
+        lastResult = result;
+
+        //Process the code
+        auto &effectManager = g_ptrSystem->EffectManager();
+        
+        auto &myEffect = effectManager.GetCurrentEffect();
+        
+
+        //As we create the switch, if we need to use variables inside the case, we enclose the case code in curley brackets
+        //    case TRIGGER_EFFECT:
+        //    {
+        //        //We can initialize variables here, including an array of the arguments passed to the effect
+        //    }
+        
+       switch (thisButton.buttonAction)
+       {
+            case BRIGHTNESS_UP:
+               deviceConfig.SetBrightness((int)deviceConfig.GetBrightness() + BRIGHTNESS_STEP);
+
+            break;
+            case BRIGHTNESS_DOWN:
+                deviceConfig.SetBrightness((int)deviceConfig.GetBrightness() - BRIGHTNESS_STEP);
+                debugI("After brightness down global brightness is  %i\n",deviceConfig.GetBrightness());
+
+            break;
+            case POWER_ON:
+                effectManager.ClearRemoteColor();
+                effectManager.SetInterval(0);
+                effectManager.SetGlobalColor(lastManualColor);
+                effectManager.StartEffect();
+            break;
+            case POWER_OFF:
+                //effectManager.SetGlobalColor(CRGB(0,0,0));
+                //g_Values.Brightness = std::max(0, (int) g_Values.Brightness - BRIGHTNESS_STEP);
+                effectManager.ClearRemoteColor();
+            break;
+            case NEXT_EFFECT: 
+                effectManager.NextEffect();
+            break;
+            case FILL_COLOR:
+                lastManualColor = hexToCrgb(thisButton.actionArgs);
+                lastManualColor.maximizeBrightness(myRemoteController.currentBrightness);
+                effectManager.SetGlobalColor(lastManualColor); 
+                //debugI("Current FastLED brightness %i\n",FastLED.getBrightness());
+                effectManager.SetInterval(0);
+            break;
+            
+            case TRIGGER_EFFECT:
+            
+            break;
+            case CHANGER:
+                if (lastManualColor.red + thisButton.actionArgs.toInt() > 255 ) {
+                    lastManualColor.red = 255;
+                } else if (lastManualColor.red + thisButton.actionArgs.toInt() < 0) {
+                    lastManualColor.red = 0;
+                } else {
+                    lastManualColor.red += thisButton.actionArgs.toInt();
+                }
+                effectManager.SetGlobalColor(lastManualColor); 
+                effectManager.SetInterval(0);
+
+            break;
+            case CHANGEG:
+                if (lastManualColor.green + thisButton.actionArgs.toInt() > 255 ) {
+                    lastManualColor.green = 255;
+                } else if (lastManualColor.green + thisButton.actionArgs.toInt() < 0) {
+                    lastManualColor.green = 0;
+                } else {
+                    lastManualColor.green += thisButton.actionArgs.toInt();
+                }
+                effectManager.SetGlobalColor(lastManualColor);
+                effectManager.SetInterval(0);
+            break;
+            case CHANGEB:
+                if (lastManualColor.blue + thisButton.actionArgs.toInt() > 255 ) {
+                    lastManualColor.blue = 255;
+                } else if (lastManualColor.blue + thisButton.actionArgs.toInt() < 0) {
+                    lastManualColor.blue = 0;
+                } else {
+                    lastManualColor.blue += thisButton.actionArgs.toInt();
+                }
+                effectManager.SetGlobalColor(lastManualColor); 
+                effectManager.SetInterval(0);
+            break;
+            case DIY1:
+               effectManager.NextEffect();
+            break;
+            case DIY2: 
+                effectManager.PreviousEffect();
+                
+                
+            break;
+            case DIY3: 
+                
+            break;
+            case DIY4: 
+                
+            break;
+
+            //case JUMP3:
+            //break;
+            //case JUMP7:
+            //break;
+            //case FADE3:
+            //break;
+            //case FADE7:
+            //break;
+            //case STROBE:
+            //break;
+            //case AUTO:
+            //break;
+            //case FLASH:
+            //break;
+            //case QUICK:
+            //break;
+            //case SLOW:
+            //break;
+       }
+  */
+
+    }
+    else
+    {
+        //There was no result, so just return.
+        debugI("We do not have a remote code result %08x", result);
+        return;
+    }
+}
+
+/*
+//The old remote handling code
 void RemoteControl::handle()
 {
     decode_results results;
@@ -128,5 +309,6 @@ void RemoteControl::handle()
         }
     }
 }
+*/
 
 #endif
